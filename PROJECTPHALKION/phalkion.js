70938fbdaa4ba856c4d9385a89a3e57c27a10f71
ryukke <78216839+ryukke@users.noreply.github.com>
2021-03-24 11:51:01 -0300

    Add files via upload

---
+++ PROJECTPHALKION/phalkion.js
@@ -0,0 +1,6 @@
+/*
+window.alert('Após esse aviso você irá se deparar com frutos da criatividade do Mestre!');
+window.confirm('Gostou das imagens e novas línguas?');
+var sugestão = window.prompt('Se não, o que nossa equipe pode trabalhar?');
+window.alert(`Aqui está a sua sugetão: ${sugestão}`);
+*/
